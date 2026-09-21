b57acdf7526e2c50bee1bd415ab998a128010146
Benjamin Avila <badjr23@hotmail.com>
2026-09-21 10:55:53 -0600

    5-hola-mundo-con-java-e-intellij-idea

---
+++ IntroduccionJava/src/HolaMundo.java
@@ -0,0 +1,8 @@
+public class HolaMundo {
+
+    // funcion main (principal)
+    public static void main(String[] args){
+        System.out.println("Hola Mundo con Java");
+        System.out.println("Saludos");
+    } // fin del metodo main
+}
